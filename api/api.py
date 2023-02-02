479b010dc050fb649077e6effb74fc53c9f5c7a4
Christian Hart <christiantaylorhart@gmail.com>
2023-02-02 13:31:00 -0500

    added mysql configuration varialbles

--- api/api.py
+++ api/api.py
@@ -1,7 +1,17 @@
 import time
-from flask import Flask
+from flask import Flask, request, jsonify
+from dotenv import find_dotenv, load_dotenv
+import os
 
 app = Flask(__name__)
+
+# DATABASE CONFIGURATION
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
+app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
+mysql = MySQL(app)
+cursor = mysql.connection.cursor()
 
 #PLACEHOLDER ROUTE
 @app.route('/time')
