c0c1f1046e01f3022cdab2848a91da1bb6cf1e7f
Christian Hart <christiantaylorhart@gmail.com>
2023-02-22 13:39:38 -0500

    fixed duplicate imports in api.py

--- api/api.py
+++ api/api.py
@@ -1,23 +1,15 @@
 import json 
-import googlemaps
-from flask import Flask
+from flask import Flask, request, jsonify
 from datetime import datetime, timedelta, timezone
 from flask_jwt_extended import create_access_token,get_jwt,get_jwt_identity, \
                                unset_jwt_cookies,jwt_required,JWTManager
+from flask_mysqldb import MySQL # Connects MySQL to Flask
 from googleroutes import get_route
-
-app = Flask(__name__)
-
-
-from flask import Flask, request, jsonify
-from dotenv import find_dotenv, load_dotenv
 import os
 import logging
-from flask_mysqldb import MySQL # Connects MySQL to Flask
-import googleroutes
-
 
 app = Flask(__name__)
+
 
 ## TOKEN CONFIG
 app.config["JWT_SECRET_KEY"] = "please-change-me"
