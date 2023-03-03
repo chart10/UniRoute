9fec6810575f5c7c8998944a34d68b6ae6add863
joshcortes <joshuacortes28@gmail.com>
2023-03-03 15:10:11 -0500

    navbar functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,122 +8,121 @@ import { CgProfile } from 'react-icons/cg';
 import { IconContext } from 'react-icons';
 
 
-function Navbar() {
+function Navbar(props) {
   const [click, setClick] = useState(false);
 
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
 
-  // if(localStorage.getItem('token') !== 'token') {
-  //   );
-  // } else {
-  //   return(
-  //   <>
-  //     <IconContext.Provider value={{ color: '#fff' }}>
-  //       <nav className='navbar'>
-  //         <Link to='/' className='navbar-logo'>
-  //           <AiOutlineDeploymentUnit className='navbar-icon' />
-  //           UNIROUTE
-  //         </Link>
-  //         <div className='menu-icon' onClick={handleClick}>
-  //           {click ? <FaTimes /> : <FaBars />}
-  //         </div>
-  //         <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-  //           <li className='nav-route'>
-  //             <NavLink
-  //               to='/'
-  //               className={({ isActive }) =>
-  //                 'nav-links' + (isActive ? ' activated' : '')
-  //               }
-  //             >
-  //               Get Route
-  //               <FaSearchLocation
-  //                 className='navbar-icon'
-  //                 onClick={closeMobileMenu}
-  //               />
-  //             </NavLink>
-  //           </li>
-           
-  //           <li className='nav-profile'>
-  //             <NavLink
-  //               to='/Profile'
-  //               className={({ isActive }) =>
-  //                 'nav-links' + (isActive ? ' activated' : '')
-  //               }
-  //             >
-  //               Your Profile
-  //               <CgProfile className='navbar-icon' onClick={closeMobileMenu} />
-  //             </NavLink>
-  //           </li>
-  //         </ul>
-  //       </nav>
-  //     </IconContext.Provider>
-  //   </>
-  // );
-  // }
-  return (  
-  <>
-      <IconContext.Provider value={{ color: '#fff' }}>
-        <nav className='navbar'>
-          <Link to='/' className='navbar-logo'>
-            <AiOutlineDeploymentUnit className='navbar-icon' />
-            UNIROUTE
-          </Link>
-          <div className='menu-icon' onClick={handleClick}>
-            {click ? <FaTimes /> : <FaBars />}
-          </div>
-          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-            <li className='nav-route'>
-              <NavLink
-                to='/'
-                className={({ isActive }) =>
-                  'nav-links' + (isActive ? ' activated' : '')
-                }
-              >
-                Get Route
-                <FaSearchLocation
-                  className='navbar-icon'
-                  onClick={closeMobileMenu}
-                />
-              </NavLink>
-            </li>
-            <li className='nav-login'>
-              <NavLink
-                to='/Login'
-                className={({ isActive }) =>
-                  'nav-links' + (isActive ? ' activated' : '')
-                }
-              >
-                Log In
-                <CgProfile className='navbar-icon' onClick={closeMobileMenu} />
-              </NavLink>
-            </li>
-            <li className='nav-profile'>
-              <NavLink
-                to='/Profile'
-                className={({ isActive }) =>
-                  'nav-links' + (isActive ? ' activated' : '')
-                }
-              >
-                Your Profile
-                <CgProfile className='navbar-icon' onClick={closeMobileMenu} />
-              </NavLink>
-            </li>
-            <li className='nav-register'>
-              <NavLink
-                to='/Register'
-                className={({ isActive }) =>
-                  'nav-links' + (isActive ? ' activated' : '')
-                }
-              >
-                Register
-                <CgProfile className='navbar-icon' onClick={closeMobileMenu} />
-              </NavLink>
-            </li>
-          </ul>
-        </nav>
-      </IconContext.Provider>
-    </>
-  )
+  if (localStorage.getItem('token') == null){
+    return (
+      <>
+        <IconContext.Provider value={{ color: '#fff' }}>
+          <nav className='navbar'>
+            <Link to='/' className='navbar-logo'>
+              <AiOutlineDeploymentUnit className='navbar-icon' />
+              UNIROUTE
+            </Link>
+            <div className='menu-icon' onClick={handleClick}>
+              {click ? <FaTimes /> : <FaBars />}
+            </div>
+            <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+              <li className='nav-route'>
+                <NavLink
+                  to='/'
+                  className={({ isActive }) =>
+                    'nav-links' + (isActive ? ' activated' : '')
+                  }
+                >
+                  Get Route
+                  <FaSearchLocation
+                    className='navbar-icon'
+                    onClick={closeMobileMenu}
+                  />
+                </NavLink>
+              </li>
+              <li className='nav-login'>
+                <NavLink
+                  to='/Login'
+                  className={({ isActive }) =>
+                    'nav-links' + (isActive ? ' activated' : '')
+                  }
+                >
+                  Log In
+                  <CgProfile className='navbar-icon' onClick={closeMobileMenu} />
+                </NavLink>
+              </li>
+              <li className='nav-profile'>
+                <NavLink
+                  to='/Profile'
+                  className={({ isActive }) =>
+                    'nav-links' + (isActive ? ' activated' : '')
+                  }
+                >
+                  Your Profile
+                  <CgProfile className='navbar-icon' onClick={closeMobileMenu} />
+                </NavLink>
+              </li>
+              <li className='nav-register'>
+                <NavLink
+                  to='/Register'
+                  className={({ isActive }) =>
+                    'nav-links' + (isActive ? ' activated' : '')
+                  }
+                >
+                  Register
+                  <CgProfile className='navbar-icon' onClick={closeMobileMenu} />
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+        </IconContext.Provider>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <IconContext.Provider value={{ color: '#fff' }}>
+          <nav className='navbar'>
+            <Link to='/' className='navbar-logo'>
+              <AiOutlineDeploymentUnit className='navbar-icon' />
+              UNIROUTE
+            </Link>
+            <div className='menu-icon' onClick={handleClick}>
+              {click ? <FaTimes /> : <FaBars />}
+            </div>
+            <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+              <li className='nav-route'>
+                <NavLink
+                  to='/'
+                  className={({ isActive }) =>
+                    'nav-links' + (isActive ? ' activated' : '')
+                  }
+                >
+                  Get Route
+                  <FaSearchLocation
+                    className='navbar-icon'
+                    onClick={closeMobileMenu}
+                  />
+                </NavLink>
+              </li>
+              <li className='nav-profile'>
+                <NavLink
+                  to='/Profile'
+                  className={({ isActive }) =>
+                    'nav-links' + (isActive ? ' activated' : '')
+                  }
+                >
+                  Your Profile
+                  <CgProfile className='navbar-icon' onClick={closeMobileMenu} />
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+        </IconContext.Provider>
+      </>
+    );
+  }
+  
 }
 export default Navbar;
