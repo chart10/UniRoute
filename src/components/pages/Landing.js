d27b35266c85c48c04d903c7549ac2162e60e29d
Chandler Dugan <duganchandler10@gmail.com>
2023-04-06 16:16:31 -0400

    added the image, welcome, and authors

--- src/components/pages/Landing.js
+++ src/components/pages/Landing.js
@@ -1,9 +1,23 @@
-
-
+import React from "react";
+import "./Landing.css";
+import img from "./images/PeopleDriving.jpg"
 
 function Landing() {
 
+    return(
+        <div class="landing-overlay">
+            <div class="landing-box">
+                <div class="image-box">
+                    <img class="landing-image" src={img}/>
+                    <div class="text-box">
+                        <h1>Welcome to UniRoute!</h1>
+                        <h3>Created By: Christian Hart, Eric Rivas and Chandler Dugan</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 
 }
 
-export default Landing();
+export default Landing;
