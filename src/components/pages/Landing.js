151476bdaf92eaca8d9b3f575e16b5f0117b8a93
Christian Hart <christiantaylorhart@gmail.com>
2023-04-20 19:05:18 -0400

    landing page work in progress

--- src/components/pages/Landing.js
+++ src/components/pages/Landing.js
@@ -1,40 +1,44 @@
-import React from "react";
-import "./Landing.css";
-import img from "./images/PeopleDriving.jpg";
-import img2 from "./images/Directions Example.PNG";
-import img3 from"./images/ProfileExample.png";
+import React from 'react';
+import './Landing.css';
+import img from './images/PeopleDriving.jpg';
+import img2 from './images/Directions Example.PNG';
+import img3 from './images/ProfileExample.png';
 
 function Landing() {
-
-    return(
-        <div class="landing-overlay">
-            <div class="landing-box">
-                <div class="image-box">
-                    <img class="landing-image" src={img}/>
-                    <div class="text-box">
-                        <h1 class="landing-text-header">Welcome to UniRoute!</h1>
-                        <h3 class="landing-text-description">Created By: Christian Hart, Eric Rivas and Chandler Dugan</h3>
-                    </div>
-                </div>
-                <div class="direction-image-box">
-                    <img class="landing-direction-image" src={img2}/>
-                    <div class="direction-image-text">
-                        <h1 class="text-header">Get Directions to Wherever you Need!</h1>
-                        <h3 class="text-description">Input your starting point, destination and arrival time to get where you need to go!</h3>
-                    </div>
-                </div>
-                <div class="profile-image-box">
-                    <div class="profile-image-text">
-                        <h1 class="text-header">Save and Edit your Profile to Quickly map your Routes!</h1>
-                        <h3 class="text-description">Save your frequent origins and destinations.</h3>
-                        <h3 class="text-description-two">Save your weekly schedule to have your routes mapped out for the week!</h3>
-                    </div>
-                    <img class='profile-image' src={img3}/>
-                </div>
-            </div>
+  return (
+    <article>
+      <img id='landing-image' src={img} />
+      <section id='landing-page1'>
+        <div id='text-box'>
+          <h1>Welcome to UniRoute!</h1>
+          <h3 id='landing-text-description'>
+            A one-page application for Univerity commuters
+          </h3>
         </div>
-    )
-
+      </section>
+      <section id='landing-page2'>
+        <img id='directions-img' src={img2} />
+        <div class='text-container'>
+          <h1>Get Directions to Wherever you Need!</h1>
+          <h3>
+            Input your starting point, destination and arrival time to get where
+            you need to go!
+          </h3>
+        </div>
+      </section>
+      <section id='landing-page3'>
+        <div class='text-container'>
+          <h1>Save and Edit your Profile to Quickly map your Routes!</h1>
+          <h3>Save your frequent origins and destinations.</h3>
+          <h3>
+            Save your weekly schedule to have your routes mapped out for the
+            week!
+          </h3>
+        </div>
+        <img id='profile-img' src={img3} />
+      </section>
+    </article>
+  );
 }
 
 export default Landing;
